6b8e69127dcad65537344fc25c711086f00a0294
Zethi <diego1900196@gmail.com>
2023-08-21 11:34:02 -0400

    add: link href and colors adjustments to main page

--- src/apps/frontend/src/app/page.tsx
+++ src/apps/frontend/src/app/page.tsx
@@ -3,25 +3,31 @@ import { Button } from "../components/ui/Button";
 
 export default function Home(): any {
   return (
-    <main className="bg-[url('/assets/backgrounds/main-page.jpg')] bg-cover bg-left px-10 md:flex md:min-h-screen md:flex-col md:items-center md:justify-center md:bg-no-repeat">
-      <h2 className="pt-16 text-7xl md:text-8xl">
+    <main className="min-h-screen bg-[url('/assets/backgrounds/main-page.jpg')] bg-cover bg-left px-10 md:flex md:flex-col md:items-center md:justify-center md:bg-no-repeat">
+      <h2 className="pt-16 text-7xl text-white md:text-8xl">
         Connect friends <b>easily & quickly</b>
       </h2>
-      <p className="mt-10 text-sm font-light text-gray-400 md:text-lg">
+      <p className="mt-10 text-sm font-light text-gray-400 text-white md:text-lg">
         Our chat app is the perfect way to stay connected with friends and family.
       </p>
 
       <div className="mt-6 flex md:gap-6">
-        <Button color="white" size="full" borderRadiusSize="md" styleClassNames="font-bold hidden md:w-56 md:block">
+        <Button
+          href="/login"
+          color="white"
+          size="full"
+          borderRadiusSize="md"
+          styleClassNames="font-bold hidden md:w-56 md:block"
+        >
           Log in
         </Button>
         <Button color="white" size="full" borderRadiusSize="md" styleClassNames="font-bold md:w-56">
           Sign up with mail
         </Button>
       </div>
-      <div className="mt-10 flex justify-center gap-1 font-light md:hidden">
+      <div className="mt-10 flex w-full justify-center gap-1 font-light text-white md:hidden">
         Existing account?{" "}
-        <Link href="" className="font-bold">
+        <Link href="/login" className="font-bold ">
           Log in
         </Link>
       </div>
